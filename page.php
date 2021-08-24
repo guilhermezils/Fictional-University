c3406c2d203db4b63440053943a601ad721f0b64
guilhermezils <guilhermezils@icloud.com>
2021-08-24 14:39:40 -0500

    parent-pg-href

--- page.php
+++ page.php
@@ -30,10 +30,11 @@
     
     
     <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+      <h2 class="page-links__title"><a href="#"><?php ;?></a></h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+        <?php 
+        wp_list_pages();
+        ?>
       </ul>
     </div>
    
